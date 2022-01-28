5c539b731b5eed57caccd62f6e314ae5f400e971
Valtteri Kurunsaari <72079744+WebVahvero@users.noreply.github.com>
2022-01-28 18:42:59 +0200

    Database connect

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Muiskismaiskis Ruokablogi | Etusivu</title>
+    <title>Muiskismaiskis Setit | Etusivu</title>
 </head>
 <body>
     <?php require 'header.php';?>
