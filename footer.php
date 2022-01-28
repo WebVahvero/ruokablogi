097eaf1a5e5171c2de219463300fb7f02bf27876
Valtteri Kurunsaari <72079744+WebVahvero@users.noreply.github.com>
2022-01-28 18:05:24 +0200

    Testi vaa bro

---
+++ footer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="styles/footer.css">
+    <link rel="stylesheet" href="styles/uni.css">
+</head>
+<body>
+    <footer>
+        <div class="some">
+            <figure>
+                <a href="#"><img class="someIcon" src="img/linkedin-in-brands.svg" alt="linkedin"></a>
+            </figure>
+            <figure>
+                <a href="https://github.com/WebVahvero"><img class="someIcon" src="img/github-brands.svg" alt="github"></a>
+            </figure>
+        </div>
+        <div class="legal">
+            <h5>Sivun tehnyt <span><a href="#">Valtteri</a></span></h5>
+            <h6><a href="#">Rekisteri</a></h6>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/uni.css">
+</head>
+<body>
+    <header>
+        <a href="index.php">
+            <div class="logo">
+                <h2 class="cursiveLogo">Valtterin</h2>
+                <h2>Muiskismaiskis</h2>
+                <h2 class="cursiveLogo">Setit</h2>
+            </div>
+        </a>
+        <nav>
+            <ul>
+                <li><a class="nav-link" href="ruokablogi.php">Ruokablogi</a></li>
+                <li><a class="nav-link" href="index.php#aboutme">Minusta</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
